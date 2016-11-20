096bd516c6aba7f5d9191a844702fba2f47ab18f
Jan Erik Hanssen <jhanssen@gmail.com>
2016-11-19 19:01:50 -0800

    more

--- setup.sh
+++ setup.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 bower install
 npm install
-patch -p1 < patches/pixi.patch
 ./node_modules/.bin/bower-requirejs -c src/config.js -e requirejs
+# patch up incorrect bower.json from pixi
+sed -i "" -e 's/bin\/pixi\.min/dist\/pixi\.min/g' src/config.js 
