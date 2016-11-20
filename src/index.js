d6e31660c4ba6253fae31543851bfe46e923e30f
Jan Erik Hanssen <jhanssen@gmail.com>
2016-11-19 20:37:40 -0800

    scroll

--- src/index.js
+++ src/index.js
@@ -8,21 +8,25 @@ function start()
     var item = new timeline.Item(100, 200, "thread 1", "some name", "some description");
     timeline.api.add(item);
 
+    var item0 = new timeline.Item(1700, 1990, "thread 1", "some name", "some description");
+    timeline.api.add(item0);
+
     var item2 = new timeline.Item(170, 270, "thread 2", "some name", "some description");
     timeline.api.add(item2);
 
-    timeline.api.setScale(4);
+    var item3 = new timeline.Item(300, 2000, "thread 2", "some name", "some description");
+    timeline.api.add(item3);
+
+    timeline.api.setScale(2);
     timeline.api.build();
 
-    setTimeout(() => {
-        timeline.api.setTime(100);
-    }, 1000);
-    setTimeout(() => {
-        timeline.api.setTime(timeline.api.end - (10 * timeline.api.scale));
-    }, 2000);
-    setTimeout(() => {
-        timeline.api.setTime(0);
-    }, 3000);
+    var scroll = document.getElementById("scroller");
+    scroll.setAttribute("style","overflow:auto;height:15px;width:" + timeline.api.width + "px");
+    var scrollInner = document.getElementById("scroller-inner");
+    scrollInner.setAttribute("style","overflow:auto;height:15px;width:" + (timeline.api.end / timeline.api.scale) + "px");
+    scroll.addEventListener('scroll', (e) => {
+        timeline.api.setTime(e.target.scrollLeft * timeline.api.scale);
+    });
 }
 
 timeline = {};
